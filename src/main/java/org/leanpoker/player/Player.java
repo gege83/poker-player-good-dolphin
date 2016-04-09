c9c04d40de03959be8660fec170bc563eec974ce
Kornel Simon <kokog.private@gmail.com>
2016-04-09 11:36:07 +0200

    1000

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,13 +1,42 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class Player {
 
 	static final String VERSION = "Default Java folding player";
 
 	public static int betRequest(JsonElement request) {
-		return 1000;
+		JsonObject obj = (JsonObject)request;
+		JsonArray players = (JsonArray) obj.get("players");
+		JsonElement inAction = obj.get("in_action");
+		JsonObject player = (JsonObject) players.get(inAction.getAsInt());
+		JsonArray holeCards = (JsonArray) player.get("hole_cards");
+		int points = 0;
+		for (int i=0; i<holeCards.size(); i++) {
+			JsonObject card = (JsonObject) holeCards.get(i);
+			String rank = card.get("rank").getAsString();
+			switch(rank) {
+			case "J":
+				points += 11;
+				break;
+			case "Q":
+				points += 12;
+				break;
+			case "K":
+				points += 13;
+				break;
+			case "A":
+				points += 14;
+				break;
+			default:
+				points += Integer.parseInt(rank);
+				break;
+			}
+		}
+		return points>14 ? 1000 : 0;
 	}
 
 	public static void showdown(JsonElement game) {
--- src/test/java/org/leanpoker/player/PlayerTest.java
+++ src/test/java/org/leanpoker/player/PlayerTest.java
@@ -1,6 +1,15 @@
 package org.leanpoker.player;
 
 import static org.junit.Assert.assertEquals;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 import org.junit.Test;
 
@@ -17,4 +26,12 @@ public class PlayerTest {
 		assertEquals(100, Player.betRequest(jsonElement));
 
 	}
+    
+    @Test
+    public void testSmoke() throws IOException, URISyntaxException {
+    	byte[] bytes = Files.readAllBytes(Paths.get(getClass().getResource("test.json").toURI()));
+    	String json = new String(bytes);
+    	JsonElement request = new JsonParser().parse(json);
+    	int result = Player.betRequest(request);
+    }
 }
