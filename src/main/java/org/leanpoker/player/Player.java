c0074010e1346f1d839a388900126f58206ee961
gege <gergo83@gmail.com>
2016-04-09 10:02:09 +0200

    go all in

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,16 +2,14 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
 
-import java.util.Map;
-
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+	static final String VERSION = "Default Java folding player";
 
-    public static int betRequest(JsonElement request) {
-        return 0;
-    }
+	public static int betRequest(JsonElement request) {
+		return 100;
+	}
 
-    public static void showdown(JsonElement game) {
-    }
+	public static void showdown(JsonElement game) {
+	}
 }
